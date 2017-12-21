00ddad16e5585da795fde61a048377a00bdb455f
Göran Krampe <goran.krampe@evothings.com>
2017-12-21 01:22:52 +0100

    Fixed feedback sending, cleaned up things, fixed paths to js libs and some more.

--- src/js/services/feedbackService.js
+++ src/js/services/feedbackService.js
@@ -1,19 +1,20 @@
-'use strict';
-angular.module('raiwApp.services').factory('feedbackService', function($http, $log, $httpParamSerializer, configService) {
-  var root = {};
-  var URL = "https://script.google.com/macros/s/AKfycbybtvNSQKUfgzgXcj3jYLlvCKrcBoktjiJ1V8_cwd2yVkpUBGe3/exec";
+'use strict'
+angular.module('raiwApp.services').factory('feedbackService', function ($http, $log, $httpParamSerializer, configService) {
+  var root = {}
+  var URL = 'http://raiw.krampe.se/feedback.php'
 
-  root.send = function(dataSrc, cb) {
-    $http(_post(dataSrc)).then(function() {
-      $log.info("SUCCESS: Feedback sent");
-      return cb();
-    }, function(err) {
-      $log.info("ERROR: Feedback sent anyway.");
-      return cb(err);
-    });
-  };
+  root.send = function (dataSrc, cb) {
+    $log.debug('DATA: ' + JSON.stringify(dataSrc))
+    $http(_post(dataSrc)).then(function () {
+      $log.info('SUCCESS: Feedback sent')
+      return cb()
+    }, function (err) {
+      $log.info('ERROR: Feedback sent anyway.')
+      return cb(err)
+    })
+  }
 
-  var _post = function(dataSrc) {
+  var _post = function (dataSrc) {
     return {
       method: 'POST',
       url: URL,
@@ -21,38 +22,33 @@ angular.module('raiwApp.services').factory('feedbackService', function($http, $l
         'content-type': 'application/x-www-form-urlencoded; charset=UTF-8'
       },
       data: $httpParamSerializer(dataSrc)
-    };
-  };
+    }
+  }
 
-  root.isVersionUpdated = function(currentVersion, savedVersion) {
+  root.isVersionUpdated = function (currentVersion, savedVersion) {
+    if (!verifyTagFormat(currentVersion)) { return 'Cannot verify the format of version tag: ' + currentVersion }
+    if (!verifyTagFormat(savedVersion)) { return 'Cannot verify the format of the saved version tag: ' + savedVersion }
 
-    if (!verifyTagFormat(currentVersion))
-      return 'Cannot verify the format of version tag: ' + currentVersion;
-    if (!verifyTagFormat(savedVersion))
-      return 'Cannot verify the format of the saved version tag: ' + savedVersion;
+    var current = formatTagNumber(currentVersion)
+    var saved = formatTagNumber(savedVersion)
+    if (saved.major > current.major || (saved.major === current.major && saved.minor > current.minor)) { return false }
 
-    var current = formatTagNumber(currentVersion);
-    var saved = formatTagNumber(savedVersion);
-    if (saved.major > current.major || (saved.major == current.major && saved.minor > current.minor))
-      return false;
+    return true
 
-    return true;
+    function verifyTagFormat (tag) {
+      var regex = /^v?\d+\.\d+\.\d+$/i
+      return regex.exec(tag)
+    }
 
-    function verifyTagFormat(tag) {
-      var regex = /^v?\d+\.\d+\.\d+$/i;
-      return regex.exec(tag);
-    };
-
-    function formatTagNumber(tag) {
-      var formattedNumber = tag.replace(/^v/i, '').split('.');
+    function formatTagNumber (tag) {
+      var formattedNumber = tag.replace(/^v/i, '').split('.')
       return {
         major: +formattedNumber[0],
         minor: +formattedNumber[1],
         patch: +formattedNumber[2]
-      };
-    };
+      }
+    }
+  }
 
-  };
-
-  return root;
-});
+  return root
+})
