9bc2d3c2fb54d90966c25e12489da6ee59f3af71
Göran Krampe <goran.krampe@evothings.com>
2018-01-07 14:46:14 +0100

    Fixed uploads

--- upload.sh
+++ upload.sh
@@ -1,23 +1,28 @@
 VER=0.1.0
-
-# Android
-scp ./platforms/android/build/outputs/apk/release/android-release-signed-aligned.apk krampe.se:/home/gokr/www/canoe/canoe-android-$VER.apk
+DESTINATION=website@getcanoe.io:/var/www/html/download/
 
 cd webkitbuilds/Canoe
 
+# Android
+cp ../../platforms/android/build/outputs/apk/release/android-release-signed-aligned.apk canoe-android-$VER.apk
+scp canoe-android-$VER.apk $DESTINATION
+
 # Linux
-mv linux64 canoe-linux64-$VER
+cp -a linux64 canoe-linux64-$VER
 zip -r canoe-linux64-$VER.zip canoe-linux64-$VER
-scp canoe-linux64-$VER.zip krampe.se:/home/gokr/www/canoe/
+scp canoe-linux64-$VER.zip $DESTINATION
 
 # OSX
-mv osx64 canoe-osx64-$VER
+cp -a osx64 canoe-osx64-$VER
 zip -r canoe-osx64-$VER.zip canoe-osx64-$VER
-scp canoe-osx64-$VER.zip krampe.se:/home/gokr/www/canoe/
+scp canoe-osx64-$VER.zip $DESTINATION
 
 # Win64
-mv win64 canoe-win64-$VER
+cp -a win64 canoe-win64-$VER
 zip -r canoe-win64-$VER.zip canoe-win64-$VER
-scp canoe-win64-$VER.zip krampe.se:/home/gokr/www/canoe/
+scp canoe-win64-$VER.zip $DESTINATION
 
-cd ..
+# Remove work stuff
+rm -rf canoe-*
+
+cd ../..
