17fa26abe72593637da2b39f26664bd024b46ac2
Rami Taibah <taibah@gmail.com>
2018-01-15 01:57:46 +0300

    Simple onboarding password creation UI implmented

---
+++ src/js/controllers/onboarding/createPassword.js
@@ -0,0 +1,4 @@
+'use strict'
+
+angular.module('canoeApp.controllers').controller('createPasswordController', function ($scope, $state, $log, $timeout, $http, $httpParamSerializer, $ionicConfig, profileService, configService, walletService, appConfigService, emailService) {
+})
