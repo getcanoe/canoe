1171b6c0b74bc8df1062ad67aca5b0fbe193821c
Göran Krampe <goran.krampe@evothings.com>
2018-02-07 13:14:34 +0100

    Clean filestorage and remove frustrating console logging

--- src/js/services/fileStorage.js
+++ src/js/services/fileStorage.js
@@ -1,157 +1,151 @@
-'use strict';
-
+'use strict'
+/* global angular FileReader LocalFileSystem cordova */
 angular.module('canoeApp.services')
-  .factory('fileStorageService', function(lodash, $log) {
-    var root = {},
-      _fs, _dir;
+  .factory('fileStorageService', function (lodash, $log) {
+    var root = {}, _fs, _dir
 
-    root.init = function(cb) {
-      if (_dir) return cb(null, _fs, _dir);
+    root.init = function (cb) {
+      if (_dir) return cb(null, _fs, _dir)
 
-      function onFileSystemSuccess(fileSystem) {
-        console.log('File system started: ', fileSystem.name, fileSystem.root.name);
-        _fs = fileSystem;
-        root.getDir(function(err, newDir) {
-          if (err || !newDir.nativeURL) return cb(err);
+      function onFileSystemSuccess (fileSystem) {
+        console.log('File system started: ', fileSystem.name, fileSystem.root.name)
+        _fs = fileSystem
+        root.getDir(function (err, newDir) {
+          if (err || !newDir.nativeURL) return cb(err)
           _dir = newDir
-          $log.debug("Got main dir:", _dir.nativeURL);
-          return cb(null, _fs, _dir);
-        });
+          $log.debug('Got main dir:', _dir.nativeURL)
+          return cb(null, _fs, _dir)
+        })
       }
 
-      function fail(evt) {
-        var msg = 'Could not init file system: ' + evt.target.error.code;
-        console.log(msg);
-        return cb(msg);
+      function fail (evt) {
+        var msg = 'Could not init file system: ' + evt.target.error.code
+        console.log(msg)
+        return cb(msg)
       };
 
-      window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, onFileSystemSuccess, fail);
-    };
+      window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, onFileSystemSuccess, fail)
+    }
 
-    root.get = function(k, cb) {
-      root.init(function(err, fs, dir) {
-        if (err) return cb(err);
+    root.get = function (k, cb) {
+      root.init(function (err, fs, dir) {
+        if (err) return cb(err)
         dir.getFile(k, {
-          create: false,
-        }, function(fileEntry) {
-          if (!fileEntry) return cb();
-          fileEntry.file(function(file) {
-            var reader = new FileReader();
+          create: false
+        }, function (fileEntry) {
+          if (!fileEntry) return cb()
+          fileEntry.file(function (file) {
+            var reader = new FileReader()
 
-            reader.onloadend = function(e) {
+            reader.onloadend = function (e) {
               return cb(null, this.result)
             }
 
-            reader.readAsText(file);
-          });
-        }, function(err) {
+            reader.readAsText(file)
+          })
+        }, function (err) {
           // Not found
-          if (err.code == 1) return cb();
-          else return cb(err);
-        });
+          if (err.code === 1) return cb()
+          else return cb(err)
+        })
       })
-    };
+    }
 
-    var writelock = {};
+    var writelock = {}
 
-    root.set = function(k, v, cb, delay) {
-
-      delay = delay || 100;
+    root.set = function (k, v, cb, delay) {
+      delay = delay || 100
 
       if (writelock[k]) {
-        return setTimeout(function() {
-          console.log('## Writelock for:' + k + ' Retrying in ' + delay);
-          return root.set(k, v, cb, delay + 100);
-        }, delay);
+        return setTimeout(function () {
+          console.log('## Writelock for:' + k + ' Retrying in ' + delay)
+          return root.set(k, v, cb, delay + 100)
+        }, delay)
       }
 
-      writelock[k] = true;
-      root.init(function(err, fs, dir) {
+      writelock[k] = true
+      root.init(function (err, fs, dir) {
         if (err) {
-          writelock[k] = false;
-          return cb(err);
+          writelock[k] = false
+          return cb(err)
         }
         dir.getFile(k, {
-          create: true,
-        }, function(fileEntry) {
+          create: true
+        }, function (fileEntry) {
           // Create a FileWriter object for our FileEntry (log.txt).
-          fileEntry.createWriter(function(fileWriter) {
-
-            fileWriter.onwriteend = function(e) {
-              console.log('Write completed:' + k);
-              writelock[k] = false;
-              return cb();
-            };
-
-            fileWriter.onerror = function(e) {
-              var err = e.error ? e.error : JSON.stringify(e);
-              console.log('Write failed: ' + err);
-              writelock[k] = false;
-              return cb('Fail to write:' + err);
-            };
-
-            if (lodash.isObject(v))
-              v = JSON.stringify(v);
-
-            if (v && !lodash.isString(v)) {
-              v = v.toString();
+          fileEntry.createWriter(function (fileWriter) {
+            fileWriter.onwriteend = function (e) {
+              // console.log('Write completed:' + k);
+              writelock[k] = false
+              return cb()
             }
 
-            $log.debug('Writing:', k, v);
-            fileWriter.write(v);
+            fileWriter.onerror = function (e) {
+              var err = e.error ? e.error : JSON.stringify(e)
+              console.log('Write failed: ' + err)
+              writelock[k] = false
+              return cb('Fail to write:' + err)
+            }
 
-          }, cb);
-        });
-      });
-    };
+            if (lodash.isObject(v)) { v = JSON.stringify(v) }
 
+            if (v && !lodash.isString(v)) {
+              v = v.toString()
+            }
+
+            // $log.debug('Writing:', k, v)
+            fileWriter.write(v)
+          }, cb)
+        })
+      })
+    }
 
     // See https://github.com/apache/cordova-plugin-file/#where-to-store-files
-    root.getDir = function(cb) {
+    root.getDir = function (cb) {
       if (!cordova.file) {
-        return cb('Could not write on device storage');
+        return cb('Could not write on device storage')
       }
 
-      var url = cordova.file.dataDirectory;
+      var url = cordova.file.dataDirectory
       // This could be needed for windows
       // if (cordova.file === undefined) {
       //   url = 'ms-appdata:///local/';
-      window.resolveLocalFileSystemURL(url, function(dir) {
-        return cb(null, dir);
-      }, function(err) {
-        $log.warn(err);
-        return cb(err || 'Could not resolve filesystem:' + url);
-      });
-    };
+      window.resolveLocalFileSystemURL(url, function (dir) {
+        return cb(null, dir)
+      }, function (err) {
+        $log.warn(err)
+        return cb(err || 'Could not resolve filesystem:' + url)
+      })
+    }
 
-    root.remove = function(k, cb) {
-      root.init(function(err, fs, dir) {
-        if (err) return cb(err);
+    root.remove = function (k, cb) {
+      root.init(function (err, fs, dir) {
+        if (err) return cb(err)
         dir.getFile(k, {
-          create: false,
-        }, function(fileEntry) {
+          create: false
+        }, function (fileEntry) {
           // Create a FileWriter object for our FileEntry (log.txt).
-          fileEntry.remove(function() {
-            console.log('File removed.');
-            return cb();
-          }, cb);
-        }, cb);
-      });
-    };
+          fileEntry.remove(function () {
+            console.log('File removed.')
+            return cb()
+          }, cb)
+        }, cb)
+      })
+    }
 
     /**
      * Same as setItem, but fails if an item already exists
      */
-    root.create = function(name, value, callback) {
+    root.create = function (name, value, callback) {
       root.get(name,
-        function(err, data) {
+        function (err, data) {
           if (data) {
-            return callback('EEXISTS');
+            return callback('EXISTS')
           } else {
-            return root.set(name, value, callback);
+            return root.set(name, value, callback)
           }
-        });
-    };
+        })
+    }
 
-    return root;
-  });
+    return root
+  })
