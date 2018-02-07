908b833627158ac719044a6d141185b70f4120d8
Göran Krampe <goran.krampe@evothings.com>
2018-02-07 12:06:41 +0100

    Fix so renaming accounts work

--- src/js/controllers/preferencesAlias.js
+++ src/js/controllers/preferencesAlias.js
@@ -3,13 +3,13 @@
 angular.module('canoeApp.controllers').controller('preferencesAliasController',
   function ($scope, $timeout, $stateParams, $ionicHistory, configService, profileService, walletService) {
     var account = profileService.getAccount($stateParams.accountId)
-    $scope.accountName = account.name
+    $scope.accountName = account.meta.label
     $scope.alias = {
       value: $scope.accountName
     }
 
     $scope.save = function () {
-      account.name = $scope.alias.value
+      account.meta.label = $scope.alias.value
       profileService.saveWallet(function () {
         $ionicHistory.goBack()
       })
