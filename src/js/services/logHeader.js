81b07c1af87f2c7da7bcbeb8a619936d4ec5f174
Göran Krampe <goran.krampe@evothings.com>
2018-02-07 10:27:51 +0100

    Cleanups

--- src/js/services/logHeader.js
+++ src/js/services/logHeader.js
@@ -1,7 +1,8 @@
-'use strict';
+'use strict'
+/* global angular */
 angular.module('canoeApp.services')
   .factory('logHeader', function($window, appConfigService, $log, platformInfo) {
-    $log.info(appConfigService.nameCase + ' v' + $window.version + ' #' + $window.commitHash);
-    $log.info('Client: ' + JSON.stringify(platformInfo));
-    return {};
-  });
+    $log.info(appConfigService.nameCase + ' v' + $window.version + ' #' + $window.commitHash)
+    $log.info('Client: ' + JSON.stringify(platformInfo))
+    return {}
+  })
