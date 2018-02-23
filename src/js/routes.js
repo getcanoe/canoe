589ab1ffee3fa8698330db70b96d2dc897434523
Göran Krampe <goran.krampe@evothings.com>
2018-02-23 13:48:22 +0100

    Added code to disable Chrome password autocomplete and more.

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,5 +1,5 @@
 'use strict'
-/* global screen cordova angular */
+/* global screen cordova angular chrome */
 
 var unsupported, isaosp
 
@@ -10,6 +10,68 @@ if (window && window.navigator) {
   if (unsupported) {
     window.location = '#/unsupported'
   }
+}
+
+/**
+ * Helper function to get the property value at path of object.
+ *
+ * @param object The object to query.
+ * @param path   The path of the property to get.
+ * @returns {*} The queried value if the path exists in the given object, otherwise undefined.
+ */
+function get (object, path) {
+  return path.split('.').reduce((acc, prop) =>
+    acc !== undefined && Object.hasOwnProperty.call(acc, prop) ? acc[prop] : undefined, object
+  )
+}
+
+/**
+* Helper function which allows to apply Chrome specific settings when running under nw.js.
+*
+* @param prop  The settings prop for which the settings should be applied. Must be passed as string.
+* @param value The value to apply to the given settings props.
+*/
+function applyChromeSetting (prop, value) {
+  const settingsObject = get(chrome, prop)
+  if (settingsObject === undefined) {
+    console.info('Setting `chrome.' + prop + '` cannot be handled by this browser.')
+    return
+  }
+  settingsObject.get({}, function (details) {
+    if (details.levelOfControl === 'controllable_by_this_extension') {
+      settingsObject.set({ value }, function () {
+        if (chrome.runtime.lastError === undefined) {
+          console.info('Successfully applied value `' + value + '` to setting `chrome.' + prop + '`')
+        } else {
+          console.error('Couldn\'t apply value `' + value + '` to setting `chrome.' + prop + '`', chrome.runtime.lastError)
+        }
+      })
+    } else {
+      console.info('Cannot control Chrome setting `chrome.' + prop + '`; LevelOfControl is: ' + details.levelOfControl)
+    }
+  })
+}
+
+/**
+* Applies the chrome.privacy settings.
+*
+* For an overview of all settings please visit the following site: https://developer.chrome.com/extensions/privacy
+*/
+function nwPrivacyConfig () {
+  console.log('HERE NOW')
+  chrome.passwordsPrivate.getSavedPasswordList(function (passwords) {
+    passwords.forEach((p, i) => {
+      chrome.passwordsPrivate.removeSavedPassword(i)
+    })
+  })
+  applyChromeSetting('privacy.services.alternateErrorPagesEnabled', false)
+  applyChromeSetting('privacy.services.autofillEnabled', false)
+  applyChromeSetting('privacy.services.hotwordSearchEnabled', false)
+  applyChromeSetting('privacy.services.passwordSavingEnabled', false)
+  applyChromeSetting('privacy.services.safeBrowsingExtendedReportingEnabled', false)
+  applyChromeSetting('privacy.services.searchSuggestEnabled', false)
+  applyChromeSetting('privacy.services.spellingServiceEnabled', false)
+  applyChromeSetting('privacy.services.translationServiceEnabled', false)
 }
 
 // Setting up route
@@ -924,6 +986,7 @@ angular.module('canoeApp').config(function (historicLogProvider, $provide, $logP
     })
 
     if (platformInfo.isNW) {
+      nwPrivacyConfig()
       var gui = require('nw.gui')
       var win = gui.Window.get()
       var nativeMenuBar = new gui.Menu({
