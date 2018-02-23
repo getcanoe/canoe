29c3b3a61ded5798c9744a7ad07b05d11df9cfe0
Göran Krampe <goran.krampe@evothings.com>
2018-02-23 13:37:16 +0100

    Revised upload script

--- upload.sh
+++ upload.sh
@@ -1,28 +1,19 @@
+# Uploads existing builds for current version in package.json
+# Also produces checksum file for all files uploaded.
+
 VER=$(node -p -e "require('./package.json').version")
 DESTINATION=website@getcanoe.io:/var/www/download/
 
-cd webkitbuilds/Canoe
+cd build
 
 # Android
-cp ../../platforms/android/build/outputs/apk/debug/android-debug.apk canoe-android-$VER-debug.apk
-scp canoe-android-$VER-debug.apk $DESTINATION
+cp ../platforms/android/build/outputs/apk/debug/android-debug.apk canoe-android-$VER-debug.apk
 
-# Linux
-cp -a linux64 canoe-linux64-$VER
-zip -r canoe-linux64-$VER.zip canoe-linux64-$VER
-scp canoe-linux64-$VER.zip $DESTINATION
+# Make sha1sum checksums
+sha256sum canoe-*-$VER*.* >> checksums-$VER.txt
 
-# OSX
-cp -a osx64 canoe-osx64-$VER
-zip -r canoe-osx64-$VER.zip canoe-osx64-$VER
-scp canoe-osx64-$VER.zip $DESTINATION
+# Upload all built files, signatures and checksums
+scp canoe-*-$VER*.* $DESTINATION
+scp checksums-$VER.txt $DESTINATION
 
-# Win64
-cp -a win64 canoe-win64-$VER
-zip -r canoe-win64-$VER.zip canoe-win64-$VER
-scp canoe-win64-$VER.zip $DESTINATION
-
-# Remove work stuff
-rm -rf canoe-*
-
-cd ../..
+cd ..
