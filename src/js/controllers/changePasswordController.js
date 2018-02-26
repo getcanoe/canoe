e27c18305abc74ce01749e2ef22d008b0aa1a5d6
Göran Krampe <goran.krampe@evothings.com>
2018-02-26 23:18:49 +0100

    Fixed change password a bit.

--- src/js/controllers/changePasswordController.js
+++ src/js/controllers/changePasswordController.js
@@ -1,10 +1,27 @@
 'use strict'
 /* global angular */
-angular.module('canoeApp.controllers').controller('changePasswordController', function ($scope, $state, $stateParams, $log, $ionicHistory, profileService, popupService, gettextCatalog) {
+angular.module('canoeApp.controllers').controller('changePasswordController', function ($scope, $state, $timeout, $log, $ionicHistory, profileService, popupService, gettextCatalog) {
+  $scope.oldPassword = ''
+  $scope.password = ''
+  $scope.confirmPassword = ''
+
   $scope.changePass = function (pw, oldPw) {
-    profileService.changePass(pw, oldPw)
-    popupService.showAlert(gettextCatalog.getString('Information'), gettextCatalog.getString('Your password has been changed'))
-    $ionicHistory.removeBackView()
-    $state.go('tabs.home')
+    if (!profileService.checkPassword(oldPw)) {
+      popupService.showAlert(gettextCatalog.getString('Information'), gettextCatalog.getString('Your old password was not entered correctly'))
+    } else {
+      profileService.changePass(pw, oldPw)
+      popupService.showAlert(gettextCatalog.getString('Information'), gettextCatalog.getString('Your password has been changed'))
+      $ionicHistory.removeBackView()
+      $state.go('tabs.home')
+    }
   }
+
+  $scope.$on('$ionicView.beforeEnter', function (event, data) {
+    $scope.oldPassword = ''
+    $scope.password = ''
+    $scope.confirmPassword = ''
+    $timeout(function () {
+      $scope.$apply()
+    })
+  })
 })
