9601250d6928f5411c0e827e3a4305f2f9acca79
Rami Taibah <taibah@gmail.com>
2018-03-19 16:51:51 +0300

    Skip button hides on password page. Still has some inconsistent behaviour though

--- src/js/controllers/onboarding/tour.js
+++ src/js/controllers/onboarding/tour.js
@@ -17,6 +17,37 @@ angular
       index: 0
     }
 
+    $scope.options = {
+      loop: false,
+      effect: 'slide',
+      speed: 500,
+      spaceBetween: 100
+    }
+
+    $scope.$on('$ionicSlides.sliderInitialized', function (event, data) {
+      $scope.slider = data.slider
+    })
+
+    $scope.$on('$ionicSlides.slideChangeStart', function (event, data) {
+      $scope.data.index = data.slider.activeIndex
+    })
+
+    $scope.goBack = function () {
+      if ($scope.data.index !== 0) {
+        $scope.slider.slidePrev()
+      } else $state.go('onboarding.welcome')
+    }
+
+    $scope.slideNext = function () {
+      if ($scope.data.index !== 3) {
+        $scope.slider.slideNext()
+      } else $state.go('onboarding.welcome')
+    }
+
+    $scope.changeSlide = function (slideIndex, speed) {
+      if ($scope.data.index !== 3) $scope.slider.slideTo(slideIndex, speed)
+    }
+
     var retryCount = 0
     $scope.createDefaultWallet = function (password) {
       // Set the password we have selected to use for our wallet
@@ -57,51 +88,5 @@ angular
           }
         )
       }, 300)
-    }
-    $scope.options = {
-      loop: false,
-      effect: 'flip',
-      speed: 500,
-      spaceBetween: 100
-    }
-
-    $scope.$on('$ionicSlides.sliderInitialized', function (event, data) {
-      $scope.slider = data.slider
-    })
-
-    $scope.$on('$ionicSlides.slideChangeStart', function (event, data) {
-      $scope.data.index = data.slider.activeIndex
-    })
-
-    $scope.$on('$ionicSlides.slideChangeEnd', function (event, data) {})
-
-    $scope.$on('$ionicView.enter', function (event, data) {
-      // This is not used anymore in onboarding
-      /* profileService.getCurrentCoinmarketcapRate(null, function (err, str) {
-        if (err) {
-          $log.warn(err)
-        } else {
-          $scope.localCurrencySymbol = '$'
-          $scope.localCurrencyPerNANO = str
-          $timeout(function () {
-            $scope.$apply()
-          })
-        }
-      }) */
-    })
-
-    $scope.goBack = function () {
-      if ($scope.data.index !== 0) $scope.slider.slidePrev()
-      else $state.go('onboarding.welcome')
-    }
-
-    $scope.slideNext = function () {
-      if ($scope.data.index !== 3) $scope.slider.slideNext()
-      else $state.go('onboarding.welcome')
-    }
-
-    $scope.changeSlide = function (slideIndex, speed) {
-      console.log('slide to')
-      $scope.slider.slideTo(slideIndex, speed)
     }
   })
