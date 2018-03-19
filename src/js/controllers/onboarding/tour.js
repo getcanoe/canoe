6984dcb4e1b9cbf384549993ade87576216f37f9
Rami Taibah <taibah@gmail.com>
2018-03-19 13:20:28 +0300

    Move password page into tour slide sequence

--- src/js/controllers/onboarding/tour.js
+++ src/js/controllers/onboarding/tour.js
@@ -1,11 +1,63 @@
 'use strict'
 /* global angular */
-angular.module('canoeApp.controllers').controller('tourController',
-  function ($scope, $state, $log, $timeout, $filter, ongoingProcess, profileService, popupService, gettextCatalog) {
+angular
+  .module('canoeApp.controllers')
+  .controller('tourController', function (
+    $scope,
+    $state,
+    $log,
+    $timeout,
+    $filter,
+    ongoingProcess,
+    profileService,
+    popupService,
+    gettextCatalog
+  ) {
     $scope.data = {
       index: 0
     }
 
+    var retryCount = 0
+    $scope.createDefaultWallet = function (password) {
+      // Set the password we have selected to use for our wallet
+      profileService.enteredPassword(password)
+      ongoingProcess.set('creatingWallet', true)
+      $timeout(function () {
+        // This is the call to create the wallet from onboarding
+        profileService.createWallet(
+          profileService.getEnteredPassword(),
+          null,
+          function (err, wallet) {
+            if (err) {
+              $log.warn(err)
+              return $timeout(function () {
+                $log.warn(
+                  'Retrying to create default wallet.....:' + ++retryCount
+                )
+                if (retryCount > 3) {
+                  ongoingProcess.set('creatingWallet', false)
+                  popupService.showAlert(
+                    gettextCatalog.getString('Cannot Create Wallet'),
+                    err,
+                    function () {
+                      retryCount = 0
+                      return $scope.createDefaultWallet()
+                    },
+                    gettextCatalog.getString('Retry')
+                  )
+                } else {
+                  return $scope.createDefaultWallet()
+                }
+              }, 2000)
+            }
+            ongoingProcess.set('creatingWallet', false)
+            $state.go('onboarding.aliasRequest', {
+              walletId: $scope.accountId
+            })
+          }
+        )
+      }, 300)
+    }
     $scope.options = {
       loop: false,
       effect: 'flip',
@@ -25,7 +77,7 @@ angular.module('canoeApp.controllers').controller('tourController',
 
     $scope.$on('$ionicView.enter', function (event, data) {
       // This is not used anymore in onboarding
-      /*profileService.getCurrentCoinmarketcapRate(null, function (err, str) {
+      /* profileService.getCurrentCoinmarketcapRate(null, function (err, str) {
         if (err) {
           $log.warn(err)
         } else {
@@ -35,7 +87,7 @@ angular.module('canoeApp.controllers').controller('tourController',
             $scope.$apply()
           })
         }
-      })*/
+      }) */
     })
 
     $scope.goBack = function () {
@@ -43,8 +95,12 @@ angular.module('canoeApp.controllers').controller('tourController',
       else $state.go('onboarding.welcome')
     }
 
+    $scope.goToSlide = function () {
+      $state.go('onboarding.welcome')
+    }
+
     $scope.slideNext = function () {
-      if ($scope.data.index !== 2) $scope.slider.slideNext()
+      if ($scope.data.index !== 3) $scope.slider.slideNext()
       else $state.go('onboarding.welcome')
     }
   })
