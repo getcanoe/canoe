a81e2426f3db924a7b4ae1ae6da5fb5c24d29be1
Tyler Storm <stormtv@mail.uc.edu>
2018-04-19 15:35:33 -0400

    Fix updating amount after send in tab recieve and tab send

--- src/js/controllers/tab-send.js
+++ src/js/controllers/tab-send.js
@@ -192,11 +192,11 @@ angular.module('canoeApp.controllers').controller('tabSendController', function 
 
   var checkSelectedAccount = function (account, accounts) {
     if (!account) return accounts[0]
-    var w = lodash.find(accounts, function (w) {
+    var w = lodash.findIndex(accounts, function (w) {
       return w.id === account.id
     })
     if (!w) return accounts[0]
-    return account
+    return accounts[w]
   }
 
   $scope.$on('$ionicView.beforeEnter', function (event, data) {
