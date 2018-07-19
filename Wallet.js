d70b75764bdaa4756a0786870f058a2d5db3daaa
Tyler Storm <stormtv@mail.uc.edu>
2018-07-18 20:45:59 -0400

    Fix Transaction Log

--- Wallet.js
+++ Wallet.js
@@ -1323,7 +1323,11 @@ module.exports = function (password) {
       aux.chain = []
       for (var j in walletData.keys[i].chain) {
         blk = newBlock()
-        blk.buildFromJSON(walletData.keys[i].chain[j])
+        var prev = null
+        if (j > 0) {
+          prev = walletData.keys[i].chain[j-1]
+        }
+        blk.buildFromJSON(walletData.keys[i].chain[j],prev)
         aux.chain.push(blk)
       }
 
